cc9ead3c07f559ddf04a7ac25f785d95a7ee10ae
kuykear <kuykear@gmail.com>
2022-05-21 12:11:23 -0500

    Complete Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string = "hey", num = Math.floor(Math.random()*1000)) {
+    let result = "";
 
+    if(num < 0){
+        result = "ERROR"
+    } else{
+        for(i = 0; i<num;i++){
+            result = result.concat(string);
+        };
+    }
+    return result;
 };
+
+console.log(repeatString());
 
 // Do not edit below this line
 module.exports = repeatString;
