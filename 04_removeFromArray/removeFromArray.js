7f4eff5304b28da7c8d031683580db5db2d6dc63
kuykear <kuykear@gmail.com>
2022-05-21 13:04:25 -0500

    Working on Exercise 3

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
 const removeFromArray = function() {
+    let array = [1,2,3,4];
+    let deletion = [2,3];
 
+    array = array.filter(item => !deletion.includes(item));
+
+    console.log(array);
+    return array;
 };
+
+removeFromArray();
 
 // Do not edit below this line
 module.exports = removeFromArray;
